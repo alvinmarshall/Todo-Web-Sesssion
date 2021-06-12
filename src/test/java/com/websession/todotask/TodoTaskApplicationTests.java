2088f0df488ce7a2908e5588c504d7d1bc2800d4
bik_krlvn <kelvinbirikorang@gmail.com>
2021-06-12 15:28:28 +0000

    :white_check_mark: add test

---
+++ src/test/java/com/websession/todotask/TodoTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websession.todotask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
